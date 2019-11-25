516cb72a68a5b53f28eadda13906c19e8ac27550
xinyeya <huafeilong2000@163.com>
2019-11-25 16:52:56 +0800

    修复侧边栏宽度不够的问题

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -4,7 +4,7 @@
       <Header></Header>
     </el-header>
     <el-container>
-      <el-aside width="180px">
+      <el-aside width="200px">
         <Side></Side>
       </el-aside>
       <el-main height="100%">
