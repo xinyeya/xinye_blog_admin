a08d308d49daea87390c33312dc7525774cdfbee
xinyeya <huafeilong2000@163.com>
2019-11-28 16:17:21 +0800

    权限修改

--- src/views/btn_right/index.vue
+++ src/views/btn_right/index.vue
@@ -4,7 +4,7 @@
       <p>我的权限：["{{admin_user}}"]</p>
       <p>
         <span>切换权限：</span>
-        <el-radio-group v-model="admin_user" size="mini">
+        <el-radio-group v-model="admin_user" size="mini" @change="handleRole">
           <el-radio-button label="editor"></el-radio-button>
           <el-radio-button label="admin"></el-radio-button>
         </el-radio-group>
@@ -27,11 +27,19 @@
 </template>
 
 <script>
+import { saveStorage } from '@/utils/storge'
 export default {
   name: 'btn_right',
   data () {
     return {
       admin_user: 'editor'
+    }
+  },
+  methods: {
+    handleRole () {
+      saveStorage(this.$store.state.role_key, this.admin_user)
+      this.$store.commit('setRole', this.admin_user)
+      console.log(this.$store.state.role)
     }
   }
 }
--- src/views/page_right/index.vue
+++ src/views/page_right/index.vue
@@ -4,7 +4,7 @@
       <p>我的权限：["admin_user"]</p>
       <p>
         <span>切换权限：</span>
-        <el-radio-group v-model="admin_user" size="mini">
+        <el-radio-group v-model="admin_user" size="mini" @change="handleRole">
           <el-radio-button label="editor"></el-radio-button>
           <el-radio-button label="admin"></el-radio-button>
         </el-radio-group>
@@ -14,11 +14,19 @@
 </template>
 
 <script>
+import { saveStorage } from '@/utils/storge'
 export default {
   name: 'page_right',
   data () {
     return {
       admin_user: 'admin'
+    }
+  },
+  methods: {
+    handleRole () {
+      saveStorage(this.$store.state.role_key, this.admin_user)
+      this.$store.commit('setRole', this.admin_user)
+      this.$router.push({ name: 'error' })
     }
   }
 }
